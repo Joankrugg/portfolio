491e0f1b0c461abae6afa86932830a80a8442816
Philippe chambon <joan_krugg@live.fr>
2021-12-31 18:34:39 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :service, :journey, :portfolio ]
 
   def home
+  end
+
+  def service
+  end
+
+  def journey
+  end
+
+  def portfolio
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'pages/journey', to: 'pages#journey'
+  get 'pages/service', to: 'pages#service'
+  get 'pages/portfolio', to: 'pages#portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
